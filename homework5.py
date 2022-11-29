ac61320c98bcb005474aca04a8a08ba94b98233d
Dmitry <chernyshov85@mail.ru>
2022-11-29 11:06:07 +0300

    чуть причесал решение 5 задачи

--- homework5.py
+++ homework5.py
@@ -11,5 +11,6 @@ a_y = int(input('Введите Y для точки A: '))
 b_x = int(input('Введите X для точки B: '))
 b_y = int(input('Введите Y для точки B: '))
 
-distance = ((a_x - b_x) * (a_x - b_x) + (a_y - b_y) * (a_y - b_y)) ** 0.5
-print(round(distance, 2))
+
+distance = str(((a_x - b_x) * (a_x - b_x) + (a_y - b_y) * (a_y - b_y)) ** 0.5).split('.')
+print(distance[0] + '.' + distance[1][:2])
